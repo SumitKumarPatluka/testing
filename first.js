2fee6cf1f77f0c54c20bc482752edf145664f60b
SumitKumarPatluka <8863sumit@gmail.com>
2025-01-15 21:44:41 -0800

    first commit

---
+++ first.js
@@ -0,0 +1,227 @@
+// fullName = "sumit kumar";
+// age=54;
+// price=85.5;
+// x = null;
+// y = undefined;
+// isFollow = true;
+// console.log(isFollow);
+
+// fullName ="sumit kumar"
+// FULLNAME ="SUMIT KUMAR"
+
+// console.log(fullName);
+// console.log(FULLNAME);
+
+// let Fullname = "sumit kumar"
+// let age = 52;
+// let totalprice = 5212;
+// console.log(Fullname);
+
+// let a;
+// a =45;
+
+// console.log(a);
+// {
+//     let a =52;
+//     console.log(a);
+// }
+
+// {
+//     let a =10;
+//     console.log(a);s
+// }
+// {
+//    FirstName ="sumit kumar"
+//    age =56;
+//     console.log();
+// }
+
+// {
+//     let age =51;
+//     console.log(age);
+// }
+// {
+//     let a =10
+//     console.log(a);
+// }
+
+
+// const student = {
+//     fullName: "sumit kumar",
+//     age: 20,
+//     cgpa: 8.2,
+//     ispass: true
+// };
+
+
+// const student ={
+// fullName: "deepak kumar",
+// age: 56,
+// paper: 2,
+// }
+
+// const student ={
+//     fullName: "rakesh kumar",
+//     age:58,
+//     paper: 54,
+
+// }
+
+// const student ={
+// fullName: "sumit kumar",
+// age: 52,
+// paper: 4,
+// }
+
+// const product ={
+//    title: "Ball pen",
+//    rating: 4, 
+//    offer: 5,
+//    price: 270,
+// };
+
+//    console.log(product);
+
+// const profile = {
+//     username: "@sumitkumar",
+//     isFollow: false,
+//     followers: 123,
+//     following: 123,
+// };
+// console.log(typeof profile["isFollow"]);
+
+// const profile = {
+//     username: "@deepakkumar"
+//     isFollow: false,
+//     followers: 321,
+//     following: 456,
+// };
+// console.log(typeof profile["isFollow"]);
+
+// let age = 15;
+
+// if (age >= 16) {
+//     console.log("vote");
+// } else {
+//     console.log("not vote");
+// }
+
+
+// odd or even 
+// let num = 10;
+
+// if (num % 2 === 5) {
+//     console.log(num,"is even");
+// } else {
+//     console.log(num,"is odd");
+// }
+
+// //syntax -> rules
+
+// let mode = "red";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// } else if (mode === "blue") {
+//     color = "blue";
+// } else if (mode === "pink") {
+//     color = "pink"
+// }else {
+//     color = "white";
+// }
+
+// console.log(color);
+
+// let age = 25;
+//  let result = age >=18 ? "adult" : "not adult"
+//  console.log (result);
+
+// let num = prompt("enter a number:");
+
+// if(num % 5 === 0) {
+//     console.log(num, "is a multiple of 5");
+// } else{
+//     console.log(num, "is not a multiple of 5");
+// }
+
+// let num = prompt("enter a number");
+
+// if(num % 5 === 0) {
+//     console.log(num, "is a multiple of");
+// } else {
+//     console.log(num, "is not a multiple of");
+// }
+
+// let score = prompt("enter a number");
+// let grade;
+
+// if(score >= 90 && score <=100) {
+//     grade = "A"
+// } else if(score >= 70 && score <= 89) {
+//     grade ="B"
+// } else if(score >= 60 && score <= 69) {
+//     grade = "C"
+// } else if(score >= 0 && score <=49) {
+//     grade = "D"
+// }
+
+// console.log("you grade was : ",grade)
+
+// for (let i = 1<=5; count = count++){
+//     console.log("sumit kumar")
+// }
+
+
+//chapter-1 Q=1
+// let a = "Harry"
+// let b = 45
+
+// console.log(a+b)
+
+//chapter-1 Q=2
+// console.log(typeof (a+b))
+
+//chapter-1 Q=3
+// const a1 = {
+//     name: "sumit",
+//     Selection: 20,
+//     isprincipal: false
+// }
+// a1['deepak'] = 'rakesh'
+// a1['bablu'] = 'manish'
+// console.log(a1)
+
+console.log("Operators in Js")
+
+let a = 45;
+let b = 4;
+
+console.log("a + b = ", a + b)
+console.log("a - b = ", a - b)
+console.log("a * b = ", a * b)
+console.log("a / b = ", a / b )
+console.log("a++ =", a++)
+console.log("a-- = ", a--)
+
+
+// Assignment Operators
+let assignment= 1;
+assignment += 5
+console.log(assignment)
+
+// Comparison operators
+
+let comp1 = 5;
+let comp2 = 6;
+console.log("comp1 > comp2 is ", comp1 > comp2)
+
+// Logical operators
+let x = 15;
+let y = 6;
+
+// console.log(x<y && x==15)
+console.log(!true)
+
+
+
